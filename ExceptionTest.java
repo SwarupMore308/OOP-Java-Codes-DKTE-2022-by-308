f113a4bbad24a4521e143c65127ad85ff786957f
Swarup More <swarup.more53@gmail.com>
2022-10-11 23:36:57 +0530

    Changes

--- ExceptionTest.java
+++ ExceptionTest.java
@@ -27,25 +27,29 @@ class Operation {
         this.num2 = num2;
     }
 
-    void addition(){
+    double addition(){
         ans = num1 + num2;
-                
+        return ans;      
     }
 
-    void subtraction(){
+    double subtraction(){
         ans = num1 - num2;
+        return ans;
     }
 
-    void division(){
+    double division(){
         ans = num1 / num2;
+        return ans;
     }
 
-    void multiplication(){
+    double multiplication(){
         ans = num1 * num2;
+        return ans;
     }
 
-    void modulo(){
+    double modulo(){
         ans = num1 % num2;
+        return ans;
     }
 
 }
@@ -58,16 +62,53 @@ class ExceptionTest {
         MyException me;
         
             while(true){
-                System.out.println("Enter your choise \n 1.Add \n2.Sub\n3.Mul \n4.div \n5.Mod\n6.exit");
-                int choice = sc.nextInt();
                 System.out.println("Enter the numbers: ");
                 double num1 = sc.nextDouble();
                 double num2 = sc.nextDouble();
                 op.setNums(num1, num2);
                 try{
-               
-
-                   
+                    double ans = op.addition();
+                    if(ans>1000){
+                        throw new MyException("TooLongAddition Exception");
+                    }
+                    System.out.println(""+op.ans);
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
+                try{
+                    double ans = op.subtraction();
+                    if(ans<0){
+                        throw new MyException("NegativeAnswer Exception");
+                    }
+                    System.out.println(""+op.ans);
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
+                try{
+                    double ans = op.multiplication();
+                    if(ans>5000){
+                        throw new MyException("TooLongMultiplication Exception");
+                    }
+                    System.out.println(""+op.ans);
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
+                try{
+                    double ans = op.division();
+                    System.out.println(""+op.ans);
+                }
+                catch(Exception e){
+                    System.out.println(e);
+                }
+                try{
+                    double ans = op.modulo();
+                    System.out.println(""+op.ans);
+                }
+                catch(Exception e){
+                    System.out.println(e);
                 }
             }
         
